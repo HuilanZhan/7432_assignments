0e599cb89b38976e5649e5cb53b7e3396329c8e3
HuilanZhan <zhanhuilan815@gmail.com>
2017-10-18 10:24:30 -0500

    deathrow.py

---
+++ deathrow.py
@@ -0,0 +1,21 @@
+import urllib2, csv
+from bs4 import BeautifulSoup
+
+output_file = open ("deathrow.csv", "w")
+writer = csv.writer(output_file)
+
+url = "https://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html"
+
+html = urllib2.urlopen(url).read()
+
+soup = BeautifulSoup(html,"html.parser") 
+
+table = soup.find("table", {"class": "tdcj_table indent"})
+
+for tr in table.find_all("tr"):
+
+		cell_list = tr.find_all("td")
+
+		data = [cell.text.encode("utf-8").strip() for cell in cell_list]
+
+		writer.writerow(data)
